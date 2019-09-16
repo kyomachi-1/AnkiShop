33cb19164c1284aab38b1ec6b5a2c46db829b9c6
kyomachi-1 <kyomachi.so.dev1@gmail.com>
2019-09-16 14:23:27 +0900

    add route for Ring Model

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,13 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+//Ring
+
+Route::get('rings/{id}', 'RingsController@show');
+Route::post('rings', 'RingsController@store');
+Route::put('rings/{id}', 'RingsController@update');
+Route::delete('rings/{id}', 'RingsController@destroy');
+Route::get('rings', 'RingsController@index')->name('rings.index');
+Route::get('rings/create', 'RingsController@create')->name('rings.create');
+Route::get('rings/{id}/edit', 'RingsController@edit')->name('rings.edit');
